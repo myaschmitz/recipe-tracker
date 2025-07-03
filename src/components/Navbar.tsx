8c3ba93a9a26f168cfcb186e77add0bbc1a0edd3
Mya Schmitz <myaschm@gmail.com>
2025-07-03 12:28:26 -0700

    navbar theme toggle updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,137 +1,3 @@
-// "use client";
-
-// import * as React from "react";
-// import Link from "next/link";
-// import { Moon, Sun, CookingPot } from "lucide-react";
-// import { useTheme } from "next-themes";
-
-// import { cn } from "@/lib/utils";
-// import {
-//   NavigationMenu,
-//   NavigationMenuItem,
-//   NavigationMenuLink,
-//   NavigationMenuList,
-// } from "@/components/ui/navigation-menu";
-// import {
-//   DropdownMenu,
-//   DropdownMenuTrigger,
-//   DropdownMenuContent,
-//   DropdownMenuItem,
-// } from "@/components/ui/dropdown-menu";
-// import { Button } from "@/components/ui/button";
-// import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-
-// export function Navbar() {
-//   const { setTheme } = useTheme();
-
-//   return (
-//     <div className="bg-inherit top-0 sticky border-b flex justify-between w-full items-center h-16">
-//       <Button
-//         variant="ghost"
-//         size="icon"
-//         className="hover:bg-transparent focus:outline-none outline-none hover:outline-none m-4 w-fit"
-//       >
-//         <Link className="flex items-center space-x-2" href="/">
-//           <CookingPot className="!w-5 !h-5" size={36} strokeWidth={3} />
-//           <span className="font-bold text-xl">recipehub</span>
-//         </Link>
-//       </Button>
-//       <NavigationMenu className="flex-1">
-//         <NavigationMenuList>
-//           <div className="flex items-center gap-2 justify-between px-4 md:px-6">
-//             <NavigationMenuItem>
-//               <Link href="/recipes" legacyBehavior passHref>
-//                 <NavigationMenuLink className="hover:font-bold">
-//                   Recipes
-//                 </NavigationMenuLink>
-//               </Link>
-//             </NavigationMenuItem>
-//           </div>
-//           <div className="flex items-center gap-2 justify-between px-4 md:px-6">
-//             <NavigationMenuItem>
-//               <Link href="/recipes/create" legacyBehavior passHref>
-//                 <NavigationMenuLink className="hover:font-bold">
-//                   Create Recipe
-//                 </NavigationMenuLink>
-//               </Link>
-//             </NavigationMenuItem>
-//           </div>
-
-//           <div className="flex items-center space-x-4">
-//             <DropdownMenu>
-//               <DropdownMenuTrigger asChild>
-//                 <Button
-//                   size="icon"
-//                   className="focus:outline-none outline-none hover:outline-none hover:bg-transparent hover:text-sky-600 dark:hover:text-sky-600 text-black shadow-none bg-transparent dark:text-white"
-//                 >
-//                   <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-//                   <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-//                   <span className="sr-only">Toggle theme</span>
-//                 </Button>
-//               </DropdownMenuTrigger>
-//               <DropdownMenuContent>
-//                 <DropdownMenuItem onClick={() => setTheme("light")}>
-//                   Light
-//                 </DropdownMenuItem>
-//                 <DropdownMenuItem onClick={() => setTheme("dark")}>
-//                   Dark
-//                 </DropdownMenuItem>
-//                 <DropdownMenuItem onClick={() => setTheme("system")}>
-//                   System
-//                 </DropdownMenuItem>
-//               </DropdownMenuContent>
-//             </DropdownMenu>
-//             <DropdownMenu>
-//               <DropdownMenuTrigger
-//                 asChild
-//                 className="border-none hover:bg-transparent"
-//               >
-//                 <button className="p-0 m-0 border-none bg-transparent outline-none hover:bg-transparent focus:outline-none">
-//                   <Avatar className="mr-4">
-//                     <AvatarImage src="https://media.timeout.com/images/106150176/1024/768/image.webp" />
-//                     <AvatarFallback>MS</AvatarFallback>
-//                   </Avatar>
-//                 </button>
-//               </DropdownMenuTrigger>
-//               <DropdownMenuContent>
-//                 <DropdownMenuItem>Profile</DropdownMenuItem>
-//                 <DropdownMenuItem>
-//                   <a href="/settings">Settings</a>
-//                 </DropdownMenuItem>
-//               </DropdownMenuContent>
-//             </DropdownMenu>
-//           </div>
-//         </NavigationMenuList>
-//       </NavigationMenu>
-//     </div>
-//   );
-// }
-
-// const ListItem = React.forwardRef<
-//   React.ElementRef<"a">,
-//   React.ComponentPropsWithoutRef<"a">
-// >(({ className, title, children, ...props }, ref) => {
-//   return (
-//     <li>
-//       <NavigationMenuLink asChild>
-//         <a
-//           ref={ref}
-//           className={cn(
-//             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-//             className
-//           )}
-//           {...props}
-//         >
-//           <div className="text-sm font-medium leading-none">{title}</div>
-//           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-//             {children}
-//           </p>
-//         </a>
-//       </NavigationMenuLink>
-//     </li>
-//   );
-// });
-// ListItem.displayName = "ListItem";
 "use client";
 
 import {
@@ -166,7 +32,7 @@ import {
   DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
-} from "@radix-ui/react-dropdown-menu";
+} from "@/components/ui/dropdown-menu";
 import { Button } from "./ui/button";
 import { useEffect, useMemo, useState } from "react";
 import { useTheme } from "next-themes";
@@ -242,12 +108,9 @@ export function Navbar() {
   return (
     <Sidebar>
       <SidebarHeader className="flex flex-row gap-2">
-        {/* <SidebarMenuItem key={"Home"}> */}
         <CookingPot />
         <span className="font-bold text-xl">recipehub</span>
-        {/* </SidebarMenuItem> */}
       </SidebarHeader>
-      {/* <Separator className="my-2" /> */}
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>General</SidebarGroupLabel>
@@ -304,30 +167,20 @@ export function Navbar() {
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <SidebarMenuButton>
-                  {/* <Button
-                    size="icon"
-                    className="focus:outline-none outline-none hover:outline-none hover:bg-transparent hover:text-sky-600 dark:hover:text-sky-600 text-black shadow-none dark:text-white"
-                  > */}
                   <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                   <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                   <span className="sr-only">Toggle theme</span>
-                  {/* </Button> */}
                 </SidebarMenuButton>
               </DropdownMenuTrigger>
-              <DropdownMenuContent className="mt-[-100%]">
-                <DropdownMenuItem onClick={() => setTheme("light")}>
-                  {/* <Button className="focus:outline-none outline-none hover:outline-none hover:bg-inherit bg-transparent p-0 m-0 text-black shadow-none dark:text-white"> */}
-                  Light
-                  {/* </Button> */}
+              <DropdownMenuContent className="min-w-[140px] shadow-md border bg-popover text-popover-foreground" align="end">
+                <DropdownMenuItem onClick={() => setTheme("light")} className="cursor-pointer hover:bg-accent hover:text-accent-foreground flex items-center gap-2 py-2 px-3"><Sun className="h-4 w-4" />
+                  <span>Light</span>
                 </DropdownMenuItem>
-                <DropdownMenuItem className="" onClick={() => setTheme("dark")}>
-                  Dark
+                <DropdownMenuItem onClick={() => setTheme("dark")} className="cursor-pointer hover:bg-accent hover:text-accent-foreground flex items-center gap-2 py-2 px-3"><Moon className="h-4 w-4" />
+                  <span>Dark</span>
                 </DropdownMenuItem>
-                <DropdownMenuItem
-                  className=""
-                  onClick={() => setTheme("system")}
-                >
-                  System
+                <DropdownMenuItem onClick={() => setTheme("system")} className="cursor-pointer hover:bg-accent hover:text-accent-foreground flex items-center gap-2 py-2 px-3"><Settings className="h-4 w-4" />
+                  <span>System</span>
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
